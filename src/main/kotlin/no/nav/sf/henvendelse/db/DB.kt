0cc8ac3759f13370c0253ee27af3f47a42785c03
Björn Hägglund <bjornhagglund@Bjorns-MacBook-Pro.local>
2024-01-10 14:37:18 +0100

    Use same isolation as sf-arkiv

--- src/main/kotlin/no/nav/sf/henvendelse/db/DB.kt
+++ src/main/kotlin/no/nav/sf/henvendelse/db/DB.kt
@@ -56,7 +56,7 @@ class PostgresDatabase {
             connectionTimeout = 250
             idleTimeout = 10001
             isAutoCommit = false
-            transactionIsolation = "READ_COMMITTED"
+            transactionIsolation = "TRANSACTION_REPEATABLE_READ"
         }
     }
 
