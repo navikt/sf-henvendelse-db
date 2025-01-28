faa62181d6958ffef03a18915b7943110569fe87
Björn Hägglund <bjorn.hagglund@nav.no>
2025-01-28 15:06:38 +0100

    DEV try redisson client

--- src/main/kotlin/no/nav/sf/henvendelse/db/token/Valkey.kt
+++ src/main/kotlin/no/nav/sf/henvendelse/db/token/Valkey.kt
@@ -1,19 +1,19 @@
 package no.nav.sf.henvendelse.db.token
 
-import io.lettuce.core.RedisClient
-import io.lettuce.core.RedisURI
-import io.lettuce.core.StaticCredentialsProvider
-import io.lettuce.core.api.StatefulRedisConnection
-import io.lettuce.core.api.sync.RedisCommands
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
-import no.nav.sf.henvendelse.db.Metrics
 import no.nav.sf.henvendelse.db.env
 import no.nav.sf.henvendelse.db.env_VALKEY_PASSWORD_HENVENDELSER
 import no.nav.sf.henvendelse.db.env_VALKEY_URI_HENVENDELSER
 import no.nav.sf.henvendelse.db.env_VALKEY_USERNAME_HENVENDELSER
+import org.redisson.Redisson
+import org.redisson.api.RMapCache
+import org.redisson.api.RedissonClient
+import org.redisson.config.Config
 import kotlin.system.measureTimeMillis
+
+const val cacheName_HENVENDELSELISTE = "henvendelseliste"
 
 object Valkey {
 
@@ -27,9 +27,9 @@ object Valkey {
         } else {
             var response: Long
             val queryTime = measureTimeMillis {
-                response = dbSize()
+                val mapCache: RMapCache<String, String> = redissonClient.getMapCache(cacheName_HENVENDELSELISTE)
             }
-            log.info { "Initial check query time $queryTime ms (got count $response)" }
+            log.info { "Initial check query time $queryTime ms" }
             if (queryTime < 100) {
                 initialCheckPassed = true
             }
@@ -37,29 +37,23 @@ object Valkey {
         }
     }
 
-    fun connectToRedis(): RedisCommands<String, String> {
-        val staticCredentialsProvider = StaticCredentialsProvider(
-            env(env_VALKEY_USERNAME_HENVENDELSER),
-            env(env_VALKEY_PASSWORD_HENVENDELSER).toCharArray()
-        )
-
-        val redisURI = RedisURI.create(env(env_VALKEY_URI_HENVENDELSER)).apply {
-            this.credentialsProvider = staticCredentialsProvider
+    fun connectToRedisson(): RedissonClient {
+        val config = Config()
+        config.useSingleServer().apply {
+            address = env(env_VALKEY_URI_HENVENDELSER) // Redis URI (e.g., redis://localhost:6379)
+            username = env(env_VALKEY_USERNAME_HENVENDELSER) // Optional: Username for Redis authentication
+            password = env(env_VALKEY_PASSWORD_HENVENDELSER) // Optional: Password for Redis authentication
         }
 
-        val client: RedisClient = RedisClient.create(redisURI)
-        val connection: StatefulRedisConnection<String, String> = client.connect()
-        return connection.sync()
+        return Redisson.create(config)
     }
 
-    val commands = connectToRedis()
-
-    fun dbSize(): Long = commands.dbsize()
+    val redissonClient = connectToRedisson()
 
     tailrec fun cacheQueryLoop() {
         runBlocking { delay(60000) } // 1 min
         try {
-            Metrics.cacheSize.set(Valkey.dbSize().toDouble())
+            // Metrics.cacheSize.set(Valkey.dbSize().toDouble())
         } catch (e: Exception) {
             log.warn { "Failed to query Redis dbSize" }
         }
