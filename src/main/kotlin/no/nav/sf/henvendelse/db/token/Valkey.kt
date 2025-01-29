d64063bdf2ba48c4a3e56d979df93b65a2637943
Björn Hägglund <bjorn.hagglund@nav.no>
2025-01-29 13:50:15 +0100

    Try redisson again

--- src/main/kotlin/no/nav/sf/henvendelse/db/token/Valkey.kt
+++ src/main/kotlin/no/nav/sf/henvendelse/db/token/Valkey.kt
@@ -5,15 +5,13 @@ import io.lettuce.core.RedisURI
 import io.lettuce.core.StaticCredentialsProvider
 import io.lettuce.core.api.StatefulRedisConnection
 import io.lettuce.core.api.sync.RedisCommands
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
 import no.nav.sf.henvendelse.db.env
 import no.nav.sf.henvendelse.db.env_VALKEY_PASSWORD_HENVENDELSER
+import no.nav.sf.henvendelse.db.env_VALKEY_URI_HENVENDELSER
 import no.nav.sf.henvendelse.db.env_VALKEY_USERNAME_HENVENDELSER
 import org.redisson.Redisson
 import org.redisson.api.RedissonClient
-import org.redisson.config.Config
 import kotlin.system.measureTimeMillis
 
 const val cacheName_HENVENDELSELISTE = "henvendelseliste"
@@ -29,7 +27,7 @@ object Valkey {
             true
         } else {
             try {
-                val redissonClient = connectToRedis()
+                val redissonClient = connectViaRedisson()
                 log.info { "Past connection" }
                 var response: Long
                 val queryTime = measureTimeMillis {
@@ -47,7 +45,7 @@ object Valkey {
         }
     }
 
-    fun connectToRedis(): RedisCommands<String, String> {
+    fun connectViaLettuce(): RedisCommands<String, String> {
         val staticCredentialsProvider = StaticCredentialsProvider(
             env(env_VALKEY_USERNAME_HENVENDELSER),
             env(env_VALKEY_USERNAME_HENVENDELSER).toCharArray()
@@ -62,52 +60,20 @@ object Valkey {
         return connection.sync()
     }
 
-    fun connectToRedisson(): RedissonClient {
-        /*
-        val config = Config()
+    fun connectViaRedisson(): RedissonClient {
+        val config = org.redisson.config.Config()
 
         val uri = env(env_VALKEY_URI_HENVENDELSER) // Example: "valkeys://valkey-teamnks-henvendelser-nav-dev.k.aivencloud.com:26483"
-        val username = env(env_VALKEY_USERNAME_HENVENDELSER)
-        val password = env(env_VALKEY_PASSWORD_HENVENDELSER)
-
-        log.info { "Attempt connection to $uri, username $username, password length ${password.length}" }
-
-        val modifiedUri = uri.replace("valkeys://", "valkeys://$username:$password@")
-
-        File("/tmp/modifiedURI").writeText(modifiedUri)
+        val usernameEnv = env(env_VALKEY_USERNAME_HENVENDELSER)
+        val passwordEnv = env(env_VALKEY_PASSWORD_HENVENDELSER)
 
         config.useSingleServer().apply {
-            address = modifiedUri
-            // username = env(env_VALKEY_USERNAME_HENVENDELSER)
-            // password = env(env_VALKEY_PASSWORD_HENVENDELSER)
+            address = uri
+            username = usernameEnv
+            password = passwordEnv
             // sslVerificationMode = SslVerificationMode.NONE // Disable strict hostname verification
             // sslProvider = SslProvider.JDK
         }
-
-         */
-        val config = Config()
-        config.useSingleServer().apply {
-            // Manually handle the connection string and SSL
-            address = "rediss://valkey-teamnks-henvendelser-nav-dev.k.aivencloud.com:26483"
-            username = env(env_VALKEY_USERNAME_HENVENDELSER)
-            password = env(env_VALKEY_PASSWORD_HENVENDELSER)
-            sslProvider = org.redisson.config.SslProvider.JDK
-            sslVerificationMode = org.redisson.config.SslVerificationMode.NONE // Optional, based on your server's SSL setup
-        }
         return Redisson.create(config)
-    }
-
-    // val redissonClient = connectToRedisson()
-
-    tailrec fun cacheQueryLoop() {
-        runBlocking { delay(60000) } // 1 min
-        try {
-            // Metrics.cacheSize.set(Valkey.dbSize().toDouble())
-        } catch (e: Exception) {
-
-            log.warn { "Failed to query Redis dbSize" }
-        }
-        runBlocking { delay(840000) } // 14 min
-        cacheQueryLoop()
     }
 }
